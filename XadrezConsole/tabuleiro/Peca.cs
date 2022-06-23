650506b04d59c3d047dbb7ffbb80f6bec8ee071b
Thiago-Ortiz <thiago.ortiz.2002@gmail.com>
2022-06-23 08:05:07 -0300

    criação da classe peça

---
+++ XadrezConsole/tabuleiro/Peca.cs
@@ -0,0 +1,17 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovim { get; protected set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+        }
+    }
+}
