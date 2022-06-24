bf4b38d5ce97197f666f3b7db7cf2a1fe359b3ce
Thiago-Ortiz <thiago.ortiz.2002@gmail.com>
2022-06-24 11:35:16 -0300

    validação movimento das peças

--- XadrezConsole/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/xadrez/PartidaDeXadrez.cs
@@ -5,18 +5,64 @@ namespace xadrez
     internal class PartidaDeXadrez
     {
         public Tabuleiro Tab { get; private set; }
-        private int Turno;
-        private Cor JogadorAtual;
+        public int Turno { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
 
         public PartidaDeXadrez()
         {
-            Tab = new Tabuleiro(8,8);
+            Tab = new Tabuleiro(8, 8);
             Turno = 1;
             JogadorAtual = Cor.Branca;
             Terminada = false;
             colocarPecas();
-            
+
+        }
+
+
+        public void validarPosicaoOrigem(Posicao pos)
+        {
+            if (Tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça na posição escolhida!");
+            }
+            if (JogadorAtual != Tab.peca(pos).Cor)
+            {
+                throw new TabuleiroException("A peça de origem não é sua!");
+            }
+            if (!Tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não há movimentos possíveis para a peça!");
+            }
+        }
+
+        public void validarPosicaoDestino(Posicao origem, Posicao destino)
+        {
+            if (!Tab.peca(origem).podeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posição de destino inválida!");
+            }
+          
+        }
+
+
+        public void realizaJogada(Posicao origem, Posicao destino)
+        {
+            executaMovimento(origem, destino);
+            Turno++;
+            mudaJogador();
+        }
+
+        private void mudaJogador()
+        {
+            if (JogadorAtual == Cor.Branca)
+            {
+                JogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                JogadorAtual = Cor.Branca;
+            }
         }
 
         public void executaMovimento(Posicao origem, Posicao destino)
@@ -29,11 +75,17 @@ namespace xadrez
 
         private void colocarPecas()
         {
-            Tab.colocarPeca(new Torre(Tab, Cor.Preta),
+            Tab.colocarPeca(new Torre(Tab, Cor.Branca),
                 new PosicaoXadrez('c', 1).toPosicao());
 
-            Tab.colocarPeca(new Rei(Tab, Cor.Preta),
+            Tab.colocarPeca(new Rei(Tab, Cor.Branca),
                 new PosicaoXadrez('d', 1).toPosicao());
+
+            Tab.colocarPeca(new Torre(Tab, Cor.Preta),
+                new PosicaoXadrez('e', 8).toPosicao());
+
+            Tab.colocarPeca(new Rei(Tab, Cor.Preta),
+                new PosicaoXadrez('d', 8).toPosicao());
         }
 
     }
